38144f948be44310ad5877531fa580f196374877
ShrimCity <tylerwhughes91@gmail.com>
2017-07-19 18:38:27 -0700

    bux fixes

--- OO_example.m
+++ OO_example.m
@@ -3,27 +3,55 @@ if (~exist('s'))
     s = simulation;
 end
 
-s.num_periods = 100;
 % compute the fields if haven't already been calculated, these are now stored in s along with other goodies
 if isempty(s.fields)
     s.compute_fields;
 end
 
+p0 = s.beta*s.me*s.c0;
 % define input phase space vector
-in = [s.dl s.W/2 s.beta*s.me*s.c0 0];
+in = [0 0 1.1*p0 0];
 % scan through different phis and draw the trajectoroes
-NP = 5;
+NP = 10;
 phis = 2*pi*rand(NP,1);
 upd = textprogressbar(NP);
 s.verbose = false;                  % turn of verbosity (dont print)
 clf; hold all;
+out_distributions = zeros(4,NP);
 for i = (1:NP)
     upd(i);
     [out, traj] = s.propagate_particle(in, phis(i));    
     plot(1e6*traj(1,:),1e6*traj(2,:))
+    out_distributions(:,i) = out;
 end
 
 xlabel('x position (um)');
 ylabel('y position (um)');
 set(findall(gcf,'type','text'),'FontSize',16,'fontWeight','normal')
 set(gca,'FontSize',16,'fontWeight','normal')
+plot([0,s.Nx*s.dl*s.num_periods*1e6],[-s.gap/2*1e6 -s.gap/2*1e6],'k');
+plot([0,s.Nx*s.dl*s.num_periods*1e6],[ s.gap/2*1e6  s.gap/2*1e6],'k');
+
+%%
+figure(2); clf;
+subplot(3,1,1)
+scatter(phis/2/pi,out_distributions(2,:)*1e6,'filled'); 
+hold all;
+set(findall(gcf,'type','text'),'FontSize',16,'fontWeight','normal')
+set(gca,'FontSize',16,'fontWeight','normal')
+ylabel('final y position (um)');
+title('transverse position')
+
+subplot(3,1,2)
+scatter(phis/2/pi,out_distributions(3,:)/p0,'filled')
+set(findall(gcf,'type','text'),'FontSize',16,'fontWeight','normal')
+set(gca,'FontSize',16,'fontWeight','normal')
+ylabel('x momentum (me \beta c_0)')
+title('longitudinal momentum')
+subplot(3,1,3)
+scatter(phis/2/pi,out_distributions(4,:)/p0,'filled')
+xlabel('light phase (2\pi)')
+set(findall(gcf,'type','text'),'FontSize',16,'fontWeight','normal')
+set(gca,'FontSize',16,'fontWeight','normal')
+ylabel('y momentum (me \beta c_0)')
+title('transverse momentum')
--- src/propagate_particle_OO.m
+++ src/propagate_particle_OO.m
@@ -13,10 +13,10 @@ function [out, trajectory] = propagate_particle_OO(obj, in, phi)
     Hz = obj.E0*obj.fields.Hz;
 
     
-    v0 = obj.beta*obj.c0;               % initial speed
+    v0 = obj.beta*obj.c0;           % initial speed
     prop_time = obj.num_periods*obj.lambda*obj.beta/v0;           % time for a straight particle to traverse structure
     
-    dt = prop_time/1000;            % time step
+    dt = obj.dl/obj.c0/obj.beta/5;  % time step
     TMAX = round(prop_time/dt*2);   % max number of times steps to consider
         
     x = x0;                         % starting x position
@@ -36,13 +36,13 @@ function [out, trajectory] = propagate_particle_OO(obj, in, phi)
         trajectory(:,T) = [x,y,px,py];
         
         xi = mod(round(x/obj.dl),obj.Nx)+1;
-        yi = mod(round(y/obj.dl),obj.Ny)+1;
+        yi = round((y/obj.dl)+obj.ny);
         
-        if (xi <= 0 || yi <= 0 || yi > obj.Ny)
+        if (xi <= 0 || y <= -obj.Ny/2*obj.dl || y > obj.Ny/2*obj.dl)
             break;
         end
         
-        if (xi > obj.Nx*obj.num_periods)
+        if (x > obj.lambda*obj.beta*obj.num_periods)
             out = [x y px py];
             break;            
         end   
